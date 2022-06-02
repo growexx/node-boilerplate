b42f52dd7c8d5b519bd44e4b9a95ac57bb2a6b34
nikhil-growexxer <nikhil.gandhi@growexx.com>
2022-06-02 19:03:27 +0530

    firebase push notification integrated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,10 +3,32 @@ const AuthMiddleWare = require('../middleware/auth');
 const ACLMiddleWare = require('../middleware/acl');
 const UploadMiddleWare = require('../middleware/upload');
 const userProfileController = require('../services/userProfile/userProfileController');
+const pushNotificationController = require('../services/pushNotification/pushNotificationController');
 
 router.get('/details', AuthMiddleWare, ACLMiddleWare, userProfileController.getUserDetails);
 router.put('/picture', AuthMiddleWare, ACLMiddleWare, UploadMiddleWare.single('photo'), userProfileController.updateProfilePicture);
 router.delete('/picture', AuthMiddleWare, ACLMiddleWare, userProfileController.deleteProfilePicture);
 router.put('/password', AuthMiddleWare, userProfileController.changePassword);
+
+// Android Push Notification
+router.post(
+    '/individual-push-notification-for-android',
+    AuthMiddleWare,
+    ACLMiddleWare,
+    pushNotificationController.postIndividualPushNotificationForAndroid);
+
+// Web Push Notification
+router.post(
+    '/individual-push-notification-for-web',
+    AuthMiddleWare, ACLMiddleWare,
+    pushNotificationController.postIndividualPushNotificationForWeb);
+router.post('/create-notification', AuthMiddleWare, ACLMiddleWare, pushNotificationController.postCreateNotification);
+router.put('/update-push-notification-token', AuthMiddleWare, ACLMiddleWare, pushNotificationController.updatePushNotificationToken);
+router.post('/send-batch-notification', AuthMiddleWare, ACLMiddleWare, pushNotificationController.postSendBatchNotification);
+router.post(
+    '/validate-push-notification-token',
+    AuthMiddleWare, ACLMiddleWare,
+    pushNotificationController.postValidatePushNotificationToken);
+
 
 module.exports = router;
