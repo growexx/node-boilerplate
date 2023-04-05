0536f91649d88f1926f0a6dad2f82f218b4ad077
hiten-growexxer <hiten.khuman@growexx.com>
2023-04-05 15:24:47 +0530

    fix: data seeding for password fixed+response messages added

--- test/seed/user.seed.js
+++ test/seed/user.seed.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const password = '400f5feac00716773753dcae08056aaf:a15de8a85f5f3637a9088a461fdd8a5263ae25c75dffef5a874498871c825617684b76734e1a5bf58609140aa1db462dd425f917d7ceff7f49eb8b285f6972dd7483a8feff3920f66319dbf118f7e982';
-
 module.exports = {
     users: [{
         _id: mongoose.Types.ObjectId('5f083c352a7908662c334532'),
@@ -10,8 +8,7 @@ module.exports = {
         isActive: 1,
         opt: 123456,
         firstName: 'user',
-        lastName: 'last',
-        password
+        lastName: 'last'
     },
     {
         _id: mongoose.Types.ObjectId('5f083c352a7908662c334535'),
@@ -19,8 +16,7 @@ module.exports = {
         employeeId: 2,
         role: 1,
         isActive: 0,
-        opt: 123456,
-        password
+        opt: 123456
     },
     {
         _id: mongoose.Types.ObjectId('5f5f2cd2f1472c3303b6b861'),
@@ -29,7 +25,6 @@ module.exports = {
         role: 4,
         isActive: 1,
         firstName: 'Test',
-        lastName: 'Admin',
-        password
+        lastName: 'Admin'
     }]
 };
