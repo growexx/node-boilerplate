7cfc9e221014967af22c21399a621f18083baf8a
Yogesh <ypanchal-parkstreet>
2023-05-20 14:27:22 +0530

    GitHub SonarQube POC

--- server/routes/sonarRoutes.js
+++ server/routes/sonarRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const sonarController = require('../services/sonar/sonarController');
 
 router.get('/codesmells', sonarController.getCodesmells);
+router.get('/vulnerabilities', sonarController.getVulnerabilities);
 
 module.exports = router;
--- server/services/sonar/sonarController.js
+++ server/services/sonar/sonarController.js
@@ -23,7 +23,8 @@ class GetSonarController {
                     'resolved': false,
                     'ps': 100,
                     'facets': 'owaspTop10,sansTop25,severities,sonarsourceSecurity,types',
-                    'additionalFields': '_all'
+                    'additionalFields': '_all',
+                    'types': 'CODE_SMELL'
                 }
             });
             const allCodeSmells = [];
@@ -36,6 +37,46 @@ class GetSonarController {
                 });
             }
             Utils.sendResponse(null, allCodeSmells, res, res.__('SUCCESS'));
+        } catch (error) {
+            Utils.sendResponse(error, null, res, '');
+        }
+    }
+
+    /**
+   * @desc This function is being used to get all the Vulnerabilities from sonarqube
+   * @author Growexx
+   * @since 19/05/2023
+   * @param {Object} req Request
+   * @param {Object} req.body RequestBody
+   * @param {function} res Response
+   */
+    static async getVulnerabilities (req, res) {
+        try {
+            const response = await axios.get('https://quality.growexx.com/api/issues/search', {
+                auth: {
+                    username: req.query.username,
+                    password: ''
+                },
+                params: {
+                    'componentKeys': req.query.componentKeys,
+                    's': 'FILE_LINE',
+                    'resolved': false,
+                    'ps': 100,
+                    'facets': 'owaspTop10,sansTop25,severities,sonarsourceSecurity,types',
+                    'additionalFields': '_all',
+                    'types': 'VULNERABILITY'
+                }
+            });
+            const allVulnerabilities = [];
+            for (const vl of response.data.issues) {
+                allVulnerabilities.push({
+                    severity: vl.severity,
+                    status: vl.status,
+                    message: vl.message,
+                    type: vl.type
+                });
+            }
+            Utils.sendResponse(null, allVulnerabilities, res, res.__('SUCCESS'));
         } catch (error) {
             Utils.sendResponse(error, null, res, '');
         }
