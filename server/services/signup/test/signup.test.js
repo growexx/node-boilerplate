eb52b9c23ef933d9db8aa75b4f9757f6f80f418d
parshva-growexxer <parshva.doshi@growexx.com>
2023-10-30 12:50:13 +0530

    refactor: add test case for signup mfa

--- server/services/signup/test/signup.test.js
+++ server/services/signup/test/signup.test.js
@@ -4,6 +4,10 @@ const expect = chai.expect;
 const assert = chai.assert;
 const request = require('supertest');
 const TestCase = require('./testcaseSignup');
+const UserVerification = require('../../../models/userVerification.model');
+const sinon = require('sinon');
+const SignUpService = require('../signUpService');
+const Email = require('../../../util/sendEmail');
 chai.use(chaiHttp);
 const trueDataStatus = 1;
 let validRegistration;
@@ -179,7 +183,39 @@ describe('Resend OTP', () => {
 
 describe('Signup Account MFA', () => {
     try {
+        let sendVerificationSMSSpy;
+        let sendVerificationEmailSpy;
+        before(() => {
+            sendVerificationSMSSpy = sinon.stub(SignUpService, 'sendVerificationSMS').resolves(true);
+            sendVerificationEmailSpy = sinon.stub(Email, 'sendVerificationEmail').resolves(true);
+        });
+
+        after(() => {
+            sendVerificationSMSSpy.restore();
+            sendVerificationEmailSpy.restore();
+        });
+
         it('As a user, I Signup the user in MFA', (done) => {
+            // Password: 123456
+            const reqObj = {
+                'email': 'admin@growexx.com',
+                'phoneNumber': '+919409805584',
+                'password': '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92',
+                'uniqueString': 'test',
+                'userType': 1
+            };
+
+            request(process.env.BASE_URL)
+                .post('/auth/signup-mfa')
+                .send(reqObj)
+                .end((err, res) => {
+                    expect(res.body.status).to.be.status;
+                    assert.equal(res.statusCode, 200);
+                    done();
+                });
+        });
+
+        it('As a user, I Signup the user in MFA is not active', (done) => {
             // Password: 123456
             const reqObj = {
                 'email': 'admin@growexx.com',
@@ -193,10 +229,32 @@ describe('Signup Account MFA', () => {
                 .send(reqObj)
                 .end((err, res) => {
                     expect(res.body.status).to.be.status;
-                    assert.equal(res.statusCode, 200);
+                    assert.equal(res.statusCode, 400);
                     done();
                 });
         });
+    } catch (exception) {
+        CONSOLE_LOGGER.error(exception);
+    }
+});
+
+describe('Verify MFA Account', async () => {
+    try {
+        let userVerificationDetails;
+        const userVerificationObj = {
+            userId: '653f52a9928bce5f1104e5ca',
+            uniqueString: '7628ffd6-ad5b-4676-93e6-0fb8d0cf76d3653f52a9928bce5f1104e5ca',
+            otp: '905211',
+            expiresAt: '2023-10-30T08:02:47.598+00:00'
+        };
+        before(() => {
+            userVerificationDetails = sinon.stub(UserVerification, 'findOne').resolves(userVerificationObj);
+        });
+
+        after(() => {
+            userVerificationDetails.restore();
+        });
+
     } catch (exception) {
         CONSOLE_LOGGER.error(exception);
     }
